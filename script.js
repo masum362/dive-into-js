9dfda8545e01678411925f38a08b085376cc329b
masum362 <masumahmed64077@gmail.com>
2024-08-17 15:23:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// variable start with var / let / const keywords
+
+// var a = 20; //var global scope variable --> not used anymore
+// let b = 20; // block scope changeable variable
+const c = 20; // block scope not changeable variable
+
+// variable declaration
+// let a;
+// const b = 20;
+
+// string --> '' , ""
+
+// const a = 'hello world!';
+// const b = 20
+// const bool = false;
+// const d = null;
+// const e = [1,2,3];
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof bool)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof f)
+
+// const numb1  = 10; //integer
+// const numb2 = 109003.4 //float
+
+// console.log(typeof numb1)
+// console.log(typeof numb2)
+
+// camelCase
+// const themeColor = 'blue';
+// // PascalCase
+// const SecondaryColor = 'red';
+// // snake_case
+// const additionanl_color_value = 'yellow';
+
+// '=' is assignment variable
+// let a = 20;
+
+// arithmatics operator
+// + , - , * , ** , / , % , ++ , --.
+// += , -= , *= , /= , %=
+
+// let a = 5;
+// const b = 2;
+
+// const sum = a + b;
+// const sub = a - b;
+// const mul = a * b;
+// const power = a ** b;
+// const div = a / b;
+// const modulas = a % b;
+
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(power);
+// console.log(div);
+// console.log(modulas);
+
+// let newValue = 6;
+// newValue += 5;
+// newValue -= 5;
+//  console.log(newValue)
+
+// type casting
+
+// string ---> int = parseInt()
+// string ---> float = parseFloat()
+// int / float ---> string = String()
+
+// const val1 = String(20);
+// const val2 = 20;
+// const sum = val1 + val2;
+// console.log(sum);
+
+
+// javascript dynamically typed language
+
+
+
+
